485ca3a0e4fd9c19ba249a93c7b68afe1145d07b
Ваше Имя <you@example.com>
2025-04-03 23:16:19 +0500

    провел порт 8281

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,6 +2,7 @@ docker stop minecraft_container
 docker rm minecraft_container
 docker run -d \
   -p 25565:25565 \
+  -p 8281:8281 \
   -v $(pwd)/world:/server/world \
   --name minecraft_container \
   m-server:latest
