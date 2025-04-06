ce387b7800889648cffd45678897f6c22fafe495
Ваше Имя <you@example.com>
2025-04-06 21:28:59 +0500

    [Backend] [fix] Исправил strat.sh и restart.sh

--- server/scripts/restart.sh
+++ server/scripts/restart.sh
@@ -2,6 +2,7 @@ docker stop minecraft_container
 docker rm minecraft_container
 docker run -d \
   -p 25565:25565 \
+  -p 8281:8281
   -v $(pwd)/:/server/ \
   --name minecraft_container \
   m-server:latest
--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -1,5 +1,6 @@
 docker run -d \
   -p 25565:25565 \
+  -p 8281:8281
   -v $(pwd)/:/server/ \
   --name minecraft_container \
   m-server:latest
